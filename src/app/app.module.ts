6f85a37911193fb6326e37df66acc9ce4c577efc
ivangomez854 <ivangomez854@gmail.com>
2022-10-09 20:12:08 -0300

    Revert "etapa 6 completa"
    This reverts commit aff5e0a1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MenuComponent } from './components/menu/menu.component';
 import { ArticulosComponent } from './components/articulos/articulos.component';
 import { ReactiveFormsModule } from  '@angular/forms';
 import { CambiarTituloPipe } from './models/cambiar-titulo.pipe';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -17,13 +18,14 @@ import { CambiarTituloPipe } from './models/cambiar-titulo.pipe';
     ArticulosFamiliasComponent,
     MenuComponent,
     ArticulosComponent,
-    CambiarTituloPipe
+    CambiarTituloPipe,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
+    NgbPaginationModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
